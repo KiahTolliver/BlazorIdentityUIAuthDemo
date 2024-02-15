b05bfa41c7b32760a98d51b8763fdeb1c4aa0bc6
blkgrlcto <adamskiah@gmail.com>
2024-02-15 07:07:42 -0500

    Add project files.

---
+++ TwitterAuthDemo/TwitterAuthDemo.Client/UserInfo.cs
@@ -0,0 +1,8 @@
+namespace TwitterAuthDemo.Client
+{
+    public class UserInfo
+    {
+        public required string UserId { get; set; }
+        public required string Email { get; set; }
+    }
+}
